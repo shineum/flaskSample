2d2b0425fe9d78960157c9001d10e7f83e653be1
shineum <shineum@gmail.com>
2022-02-11 08:28:36 -0800

    add test path

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/test")
+def path_test():
+    return '<p>test</p>'
+
 @app.route("/")
 def hello_world():
     return '<p>Hello World</p>'
